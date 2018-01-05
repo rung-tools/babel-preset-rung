9da78ad829f09f7fcf3033ce87f77aa5e3c180b9
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2018-01-05 11:00:46 -0200

    Use optional dependencies

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 
-module.exports = (_, opts = { browser: false }) => ({
-    presets: opts.browser
+const bluebirdPlugin = require('bluebird')
+    ? [require('babel-plugin-transform-async-to-bluebird').default]
+    : []
+
+const dateFnsPlugin = require('date-fns')
+    ? [require('babel-plugin-date-fns').default]
+    : []
+
+module.exports = () => ({
+    presets: require('react')
         ? [require('babel-preset-env'), require('babel-preset-react')]
         : [[require('babel-preset-env'), { targets: { node: 'current' } }]],
     plugins: [
         require('babel-plugin-implicit-function').default,
         require('babel-plugin-function-composition').default,
         require('@rung/babel-plugin-pipe-operator').default,
-        require('babel-plugin-transform-async-to-bluebird').default,
         require('babel-plugin-holes').default,
-        require('babel-plugin-date-fns').default] })
+        ...bluebirdPlugin,
+        ...dateFnsPlugin] })
